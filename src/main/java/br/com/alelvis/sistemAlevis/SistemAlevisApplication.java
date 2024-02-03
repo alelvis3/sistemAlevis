1a1c2132121fc9895f695bb326f52071100cc169
alelvis3 <alelvis3br@hotmail.com>
2024-02-03 14:31:05 -0300

    Spring Boot

---
+++ src/main/java/br/com/alelvis/sistemAlevis/SistemAlevisApplication.java
@@ -0,0 +1,13 @@
+package br.com.alelvis.sistemAlevis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemAlevisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemAlevisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/alelvis/sistemAlevis/SistemAlevisApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alelvis.sistemAlevis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemAlevisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
