8f5bd8b94201d9c6830326b3380335c0baae69c7
alelvis3 <alelvis3br@hotmail.com>
2024-02-03 15:03:29 -0300

    Ajustando frame

--- src/main/java/br/com/alelvis/sistemAlevis/SistemAlevisApplication.java
+++ src/main/java/br/com/alelvis/sistemAlevis/SistemAlevisApplication.java
@@ -1,13 +1,16 @@
 package br.com.alelvis.sistemAlevis;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SistemAlevisApplication {
-
 	public static void main(String[] args) {
-		SpringApplication.run(SistemAlevisApplication.class, args);
+		// SpringApplication.run(SistemAlevisApplication.class, args);
+		ApplicationContext context = new SpringApplicationBuilder(SistemAlevisApplication.class).headless(false)
+				.run(args);
+		TelaLogin frame = context.getBean(TelaLogin.class);
+		frame.setVisible(true);
 	}
-
 }
